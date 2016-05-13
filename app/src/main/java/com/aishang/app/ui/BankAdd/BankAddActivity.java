f974a54cce6743b3c10b81845f2e58756d521bbe
find-happiness <w564263331@gmail.com>
2016-05-13 11:23:32 +0800

    1.修改了解我们 2.修改添加银卡

--- app/src/main/java/com/aishang/app/ui/BankAdd/BankAddActivity.java
+++ app/src/main/java/com/aishang/app/ui/BankAdd/BankAddActivity.java
@@ -2,14 +2,11 @@ package com.aishang.app.ui.BankAdd;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.InputType;
 import android.text.TextUtils;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.RelativeLayout;
@@ -31,6 +28,7 @@ public class BankAddActivity extends AppCompatActivity {
   @Bind(R.id.holder) TextView holder;
   @Bind(R.id.accountNumber) TextView accountNumber;
   @Bind(R.id.layoutRoot) RelativeLayout layoutRoot;
+  @Bind(R.id.aphle_btn) View rlBank;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -40,16 +38,16 @@ public class BankAddActivity extends AppCompatActivity {
     initEtBankName();
   }
 
-  @Override public boolean onCreateOptionsMenu(Menu menu) {
-    MenuInflater inflater = getMenuInflater();
-    inflater.inflate(R.menu.menu_save, menu);
-    return true;
-  }
-
   private void initEtBankName() {
     bankName.setInputType(InputType.TYPE_NULL);
 
-    bankName.setOnClickListener(new View.OnClickListener() {
+    bankName.setOnTouchListener(new View.OnTouchListener() {
+      @Override public boolean onTouch(View v, MotionEvent event) {
+        return true;
+      }
+    });
+
+    rlBank.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View v) {
         DialogFactory.createSingleChoiceDialog(BankAddActivity.this, R.array.bank, -1,
             new DialogInterface.OnClickListener() {
@@ -67,42 +65,6 @@ public class BankAddActivity extends AppCompatActivity {
   private void initToolbar() {
     toolbar.setTitle("");
     this.setSupportActionBar(toolbar);
-    toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-      @Override public boolean onMenuItemClick(MenuItem item) {
-        if (item.getItemId() == R.id.action_save) {
-
-          if (isAccountNumberEmpty()) {
-            showError("银行帐号不能为空！");
-            return true;
-          }
-
-          if (isBankHolderEmpty()) {
-            showError("持卡人不能为空！");
-            return true;
-          }
-
-          if (isBankNameEmpty()) {
-            showError("银行名字不能为空！");
-            return true;
-          }
-
-          Intent intent = new Intent();
-
-          JMemberBankAccount account = new JMemberBankAccount();
-          account.setAccountNumber(accountNumber.getText().toString().trim());
-          account.setBankName(bankName.getText().toString().trim());
-          account.setHolder(holder.getText().toString().trim());
-          account.setId(0);
-          intent.putExtra(ACCOUNT, account);
-
-          BankAddActivity.this.setResult(100,
-              intent);// 设置回传数据。resultCode值是1，这个值在主窗口将用来区分回传数据的来源，以做不同的处理
-          BankAddActivity.this.finish();// 关闭子窗口
-          return true;
-        }
-        return false;
-      }
-    });
     toolbar.setNavigationIcon(R.mipmap.iconfont_livesvg);
     toolbar.setNavigationOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View v) {
@@ -125,5 +87,35 @@ public class BankAddActivity extends AppCompatActivity {
 
   private void showError(String error) {
     CommonUtil.showSnackbar(error, layoutRoot);
+  }
+
+  @OnClick(R.id.submint) public void onClick() {
+
+    if (isAccountNumberEmpty()) {
+      showError("银行帐号不能为空！");
+      return;
+    }
+
+    if (isBankHolderEmpty()) {
+      showError("持卡人不能为空！");
+      return;
+    }
+
+    if (isBankNameEmpty()) {
+      showError("银行名字不能为空！");
+      return;
+    }
+
+    Intent intent = new Intent();
+
+    JMemberBankAccount account = new JMemberBankAccount();
+    account.setAccountNumber(accountNumber.getText().toString().trim());
+    account.setBankName(bankName.getText().toString().trim());
+    account.setHolder(holder.getText().toString().trim());
+    account.setId(0);
+    intent.putExtra(ACCOUNT, account);
+
+    BankAddActivity.this.setResult(100, intent);// 设置回传数据。resultCode值是1，这个值在主窗口将用来区分回传数据的来源，以做不同的处理
+    BankAddActivity.this.finish();// 关闭子窗口
   }
 }
